e80a2bcb3d0ba414889ca265d66f326e8674dc11
Neven <58472164+neven14@users.noreply.github.com>
2022-01-08 09:50:33 +0100

    Add files via upload

---
+++ Prvi_Zad/Prvi_Zad/KlasaC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prvi_Zad
+{
+    class KlasaC
+    {
+        public int Zbroj(int[] arr)
+        {
+            int zbr = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                zbr += arr[i];
+            }
+            return zbr;
+        }
+        public double Prosjek(int[] prosjek)
+        {
+            int sum = 0;
+            for (int i = 0; i < prosjek.Length; i++)
+            {
+                sum += prosjek[i];
+            }
+         return sum / prosjek.Length;
+        }
+    }
+}
---
+++ Prvi_Zad/Prvi_Zad/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prvi_Zad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zbrojPros = new int[5];
+            Console.WriteLine("Upisite 5 cijelih brojeva:");
+            for (int i = 0; i < 5; i++)
+            {
+                string s = Console.ReadLine();
+                zbrojPros[i] = Convert.ToInt32(s);
+            }
+            KlasaC x = new KlasaC();
+            int zbroj = x.Zbroj(zbrojPros);
+            double prosjek = x.Prosjek(zbrojPros);
+            Console.WriteLine("Zbroj " + zbroj + ", prosjek " + prosjek);
+        }
+    }
+}
